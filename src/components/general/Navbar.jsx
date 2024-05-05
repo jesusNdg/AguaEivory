88db7a6e7447b442efcf3ed835f7a1ad416347fb
Alexander0127 <AlexanderTR0127@gmail.com>
2024-05-05 00:21:49 -0500

    nav bar

--- src/components/general/Navbar.jsx
+++ src/components/general/Navbar.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div className='w-full h-24 bg-sky-300 '>
-        <div>logo</div>
-        <ul className='flex gap-32 justify-center h-full w-full items-center'>
+    <div className='w-full h-12 bg-gray-200 flex items-center justify-around'>
+        <div className='w-48'>logo</div>
+        <ul className='flex justify-around h-full w-96 items-center'>
         <li><a href="">Inicio</a></li>
         <li><a href="">Productos</a></li>
         <li><a href="">Contactos</a></li>
         </ul>
-        <div>Carrito de compras</div>
+        <div className='w-48'>Carrito de compras</div>
     </div>
   )
 }
